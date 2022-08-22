c2d7414aab6be62048855409975772fb1c39281f
Zeromemer <zeromemer@users.noreply.github.com>
2022-08-22 22:59:16 +0300

    removed full screen button from clock

--- clock/index.js
+++ clock/index.js
@@ -1,6 +1,5 @@
 import tick from '../stuff/tick.js'
 const time = document.getElementById('time');
-const fullScreenButton = document.getElementById('fullscreen_btn');
 
 function pad(n) {
     return String(n).padStart(2, '0');
@@ -11,11 +10,3 @@ tick(() => {
     time.innerText = `${pad(date.getDate())}/${pad(date.getMonth())}/${pad(date.getFullYear())}, ` + 
         `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
 }, 1000);
-
-let fullScreen = false;
-fullScreenButton.onclick = () => {
-    console.log(fullScreen);
-    if (document.fullscreenElement === null) document.body.requestFullscreen();
-    else document.exitFullscreen();
-    fullScreen = !fullScreen;
-}
