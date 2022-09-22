ae7f2fab3dd642a0218f993749e6ecd82b81bfe6
Zeromemer <zeromemer@users.noreply.github.com>
2022-09-22 19:04:31 +0300

    improved tick.js

--- stuff/tick.js
+++ stuff/tick.js
@@ -5,7 +5,7 @@
  * @param {number} delay
 */
 export default function tick(callback, delay) {
-    const now = Date.now();
     callback();
+    const now = Date.now();
     setTimeout(() => tick(callback, delay), (Math.round(now / delay) * delay + delay) - now);
 }
