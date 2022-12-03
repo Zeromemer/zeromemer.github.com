ef2d372779b4bbc0b9961130ddb797978444e995
Zeromemer <zeromemer@users.noreply.github.com>
2022-12-03 20:16:50 +0200

    added fullscreen toggle and options for stoping and starting the clock

--- clock/index.js
+++ clock/index.js
@@ -5,8 +5,62 @@ function pad(n) {
     return String(n).padStart(2, '0');
 }
 
-tick(() => {
+const ticker = tick(() => {
     const date = new Date();
     time.innerText = `${pad(date.getDate())}/${pad(date.getMonth() + 1)}/${pad(date.getFullYear())}, ` + 
         `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
 }, 1000);
+
+function toggleFullScreen() {
+    if (!document.fullscreenElement &&    // alternative standard method
+        !document.mozFullScreenElement && !document.webkitFullscreenElement) {  // current working methods
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+    } else {
+        if (document.cancelFullScreen) {
+            document.cancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }
+    }
+}
+
+const phrases = [
+    [ 'start', function() { ticker.paused = false; } ],
+    [ 'stop', function() { ticker.paused = true; } ],
+];
+const progressMap = new Map(phrases.map(phrase => [phrase[0], 0]));
+
+document.addEventListener('keydown', (e) => {
+    const key = e.key.toLowerCase();
+
+    if (key.toLowerCase() === 'f') {
+        if (!ticker.paused) {
+            toggleFullScreen();
+        }
+    }
+
+    phrases.forEach(([phrase, handler]) => {
+        const progress = progressMap.get(phrase);
+
+        if (phrase[progress] === key) {
+            progressMap.set(phrase, progress + 1);
+        } else {
+            progressMap.set(phrase, 0);
+        }
+
+        if (phrase.length === (progress + 1)) {
+            handler();
+            progressMap.forEach((v, k) => progressMap.set(k, 0));
+        }
+
+        console.log(phrase, progress);
+    });
+})
