359e1efb4280e680e2ea4d9298fbf16373f6206a
Zeromemer <zeromemer@users.noreply.github.com>
2022-12-04 23:20:41 +0200

    moved Data class from /enc/index.js to /stuff/data.js

--- enc/index.js
+++ enc/index.js
@@ -1,48 +1,4 @@
-class Data {
-    #data;
-    static encoder = new TextEncoder();
-    static decoder = new TextDecoder();
-
-    constructor(data) {
-        if (data instanceof Uint8Array) {
-            this.#data = data;
-        } else {
-            throw new TypeError('data can only be an instance of the Uint8Array class.');
-        }
-    }
-
-    static fromUint8Array(arr) {
-        return new Data(arr);
-    }
-
-    static fromString(string) {
-        return new Data(Data.encoder.encode(string));
-    }
-
-    static fromArrayBuffer(arrBuf) {
-        return new Data(new Uint8Array(arrBuf));
-    }
-
-    static fromHex(string) {
-        return new Data(new Uint8Array(string.match(/.{1,2}/g).map(s => parseInt(s, 16))));
-    }
-
-    get uint8Array() {
-        return this.#data;
-    }
-
-    get string() {
-        return Data.decoder.decode(this.#data);
-    }
-    
-    get arrayBuffer() {
-        return this.#data.buffer;
-    }
-    
-    get hex() {
-        return Array.from(this.#data).map(i => i.toString(16).padStart(2, '0')).join('');
-    }
-}
+import Data from "../stuff/data.js";
 
 const salt = Data.fromString('This can be anything, and I have chosen for it to be this sentence. (amogus)');
 
---
+++ stuff/data.js
@@ -0,0 +1,51 @@
+/** Binary data wraper class */
+export default class Data {
+    #data;
+    static encoder = new TextEncoder();
+    static decoder = new TextDecoder();
+
+    /** @param {Uint8Array} data */
+    constructor(data) {
+        if (data instanceof Uint8Array) {
+            this.#data = data;
+        } else {
+            throw new TypeError('data can only be an instance of the Uint8Array class.');
+        }
+    }
+
+    /** @param {Uint8Array} arr  */
+    static fromUint8Array(arr) {
+        return new Data(arr);
+    }
+
+    /** @param {string} string */
+    static fromString(string) {
+        return new Data(Data.encoder.encode(string));
+    }
+
+    /** @param {ArrayBuffer} arrBuf */
+    static fromArrayBuffer(arrBuf) {
+        return new Data(new Uint8Array(arrBuf));
+    }
+
+    /** @param {string} string */
+    static fromHex(string) {
+        return new Data(new Uint8Array(string.match(/.{1,2}/g).map(s => parseInt(s, 16))));
+    }
+
+    get uint8Array() {
+        return this.#data;
+    }
+
+    get string() {
+        return Data.decoder.decode(this.#data);
+    }
+    
+    get arrayBuffer() {
+        return this.#data.buffer;
+    }
+    
+    get hex() {
+        return Array.from(this.#data).map(i => i.toString(16).padStart(2, '0')).join('');
+    }
+}
